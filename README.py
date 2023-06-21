cf6ab32d2399f0db7d31440c10e797e11d24615d
Marcelo Eugênio <123710944+CeloEug@users.noreply.github.com>
2023-06-20 21:52:36 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,15 +1,1 @@
-#Time:
-#Caio Braga
-#Davi Lopes
-#Júnior Miranda
-#Marcelo Eugênio
-
-#Uma tela de login com banco de dados e um gerador de senhas automático. Até agora com as seguintes funções:
-#-Requição de uma palavra aleatória de https://www.mit.edu/~ecprice/wordlist.10000.
-#-Substituição das vogais por caracteres especiais correspondentes, ex: a = @.
-#-front end com customTkinter fora da janela padrão do windows tkinter.
-
-#A login screen with a database and a random password generator. 
-#-requests a random word from the mit api.
-#-substitutes the vocals for their special charater counterparts.
 
